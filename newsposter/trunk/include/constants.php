5b777ecb25a54b94f75c949d377753bd7cc375af
mrfrost <mrfrost@2db7d633-71b8-0310-b1c1-c0b6732fd93e>
2003-02-13 18:36:49 +0000

    - changed version constant to 0.5.0
    git-svn-id: file:///home/frank/srv/svn@61 2db7d633-71b8-0310-b1c1-c0b6732fd93e

--- newsposter/trunk/include/constants.php
+++ newsposter/trunk/include/constants.php
@@ -8,7 +8,7 @@
  */
 
 // misc
-define('VERSION'   , '0.5.0-rc1');
+define('VERSION'   , '0.5.0');
 define('CODENAME'  , 'Odyssee');
 define('USER_AGENT', 'User-Agent: Newsposter/'.VERSION.' ("'.CODENAME.'")');
 
